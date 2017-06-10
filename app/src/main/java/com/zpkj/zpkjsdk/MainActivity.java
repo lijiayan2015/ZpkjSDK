18d64ac1c12594dda21f3e6c83f695f0113956f6
lijiayan2015 <lijiayan_mail@163.com>
2017-06-10 14:55:06 +0800

    zpkj company's tool and api create by ljy

--- app/src/main/java/com/zpkj/zpkjsdk/MainActivity.java
+++ app/src/main/java/com/zpkj/zpkjsdk/MainActivity.java
@@ -1,9 +1,7 @@
 package com.zpkj.zpkjsdk;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
-import com.zpkj.okhttputils.LogUtils;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
